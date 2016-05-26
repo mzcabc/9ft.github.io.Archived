63b1d353e3f29432052571c9fdbbac3b567975f3
Mindy Cong <mindycong@gmail.com>
2016-05-27 01:25:53 +0800

    Jekyll: Theme: switch to 3-Jekyll Github: P233/3-Jekyll https://github.com/P233/3-Jekyll

---
+++ _site/assets/js/main.js
@@ -0,0 +1,66 @@
+$(function() {
+  var toc     = $('.toc-link'),
+      sidebar = $('#sidebar'),
+      main    = $('#main'),
+      menu    = $('#menu'),
+      x1, y1;
+
+  // run this function after pjax load.
+  var afterPjax = function() {
+    // open links in new tab.
+    $('#main').find('a').filter(function() {
+      return this.hostname != window.location.hostname;
+    }).attr('target', '_blank');
+
+    // discus comment.
+    
+
+    // your scripts
+  };
+  afterPjax();
+
+
+  // NProgress
+  NProgress.configure({ showSpinner: false });
+
+
+  // Pjax
+  $(document).pjax('#sidebar-avatar, .toc-link', '#main', {
+    fragment: '#main',
+    timeout: 3000
+  });
+
+  $(document).on({
+    'pjax:click': function() {
+      NProgress.start();
+      main.removeClass('fadeIn');
+    },
+    'pjax:end': function() {
+      afterPjax();
+      NProgress.done();
+      main.scrollTop(0).addClass('fadeIn');
+      menu.add(sidebar).removeClass('open');
+      
+    }
+  });
+
+
+  // Tags Filter
+  $('#sidebar-tags').on('click', '.sidebar-tag', function() {
+    var filter = $(this).data('filter');
+    if (filter === 'all') {
+      toc.fadeIn(350);
+    } else {
+      toc.hide();
+      $('.toc-link[data-tags~=' + filter + ']').fadeIn(350);
+    }
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+
+
+  // Menu
+  menu.on('click', function() {
+    $(this).add(sidebar).toggleClass('open');
+  });
+
+});
